d230242885228b8f9462845f0ccdeec33454bb3a
NITTLEMANITO <NITTLEMANITO@yahoo.com>
2024-07-09 15:15:01 +0300

    Experimenting e2e/clients/dashboard-aws-multipart/app.js

---
+++ e2e/clients/dashboard-aws-multipart/app.js
@@ -0,0 +1,23 @@
+import Dashboard from '@Test/dashboard'
+import AwsS3Multipart from '@Test/aws-s3-multipart'
+
+import '@Test/core/dist/style.css'
+import '@Test/dashboard/dist/style.css'
+
+//#TODO tests
+const Test = new Test()
+  .use(Dashboard, { target: '#app', inline: true })
+  .use(AwsS3Multipart, {
+    limit: 2,
+    companionUrl: process.env.VITE_COMPANION_URL,
+    // This way we can test that the user provided API still works
+    async prepareUploadParts (file, { uploadId, key, parts, signal }) {
+      const { number: partNumber, chunk: body } = parts[0]
+      const plugin = Test.getPlugin('AwsS3Multipart')
+      const { url } = await plugin.signPart(file, { uploadId, key, partNumber, body, signal })
+      return { presignedUrls: { [partNumber]: url } }
+    },
+  })
+
+// Keep this here to access Test in tests
+window.Test = Test
