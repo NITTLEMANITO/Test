194fcbe88d3cf021ff46e58752ef2226281ebd62
NITTLEMANITO <NITTLEMANITO@yahoo.com>
2024-08-06 11:40:02 +0300

    Optimizing performance of e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :Test="Test" />
+</template>
+
+<script>
+import Vue from 'vue'
+import Test from '@Test/core'
+import { Dashboard } from '@Test/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    Test: () => new Test(),
+  },
+}
+</script>
+
+<style src="@Test/core/dist/style.css"></style>
+<style src="@Test/dashboard/dist/style.css"></style>
